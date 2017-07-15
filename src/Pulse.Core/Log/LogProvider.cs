2ecd854301a29eaaf39f079c15085f5ac8d4ec1a
Vedran Vučetić <vedran.vucetic@gmail.com>
2017-07-15 12:24:35 +0200

    fixed gitignore ignoring .cs files in Log folder

---
+++ src/Pulse.Core/Log/LogProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pulse.Core.Log
+{
+    public static class LogProvider
+    {
+        private static ILog _currentLogger = new ConsoleLogger();
+
+        public static void SetCurrentLogger(ILog logger)
+        {
+            _currentLogger = logger;
+        }
+
+        public static ILog GetLogger()
+        {
+            return _currentLogger;
+        }
+    }
+}
