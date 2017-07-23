fbf020f53e7b2b412d4080590b3c03061d626dfa
Vedran Vučetić <vedran.vucetic@gmail.com>
2017-07-24 00:15:49 +0200

    Refactor logger interface

--- src/Pulse.Core/Log/ConsoleLogger.cs
+++ src/Pulse.Core/Log/ConsoleLogger.cs
@@ -8,31 +8,6 @@ namespace Pulse.Core.Log
 {
     public class ConsoleLogger : ILog
     {
-        public void Debug(Exception ex, string message)
-        {
-            System.Diagnostics.Debug.WriteLine($"{message} : {ex}");
-        }
-
-        public void Debug(string message)
-        {
-            System.Diagnostics.Debug.WriteLine($"{message}");
-        }
-
-        public void Error(Exception ex, string message)
-        {
-            System.Diagnostics.Debug.WriteLine($"{message} : {ex}");
-        }
-
-        public void Fatal(Exception ex, string message)
-        {
-            System.Diagnostics.Debug.WriteLine($"{message} : {ex}");
-        }
-
-        public void Information(string message)
-        {
-            System.Diagnostics.Debug.WriteLine($"{message}");
-        }
-
         public void Log(LogLevel level, string message, Exception ex)
         {
             System.Diagnostics.Debug.WriteLine($"{level.ToString().ToUpper()} - {message} : {ex}");
@@ -43,11 +18,6 @@ namespace Pulse.Core.Log
             System.Diagnostics.Debug.WriteLine($"{level.ToString().ToUpper()} - {message}");
         }
         
-        public void Warning(Exception ex, string message)
-        {
-            System.Diagnostics.Debug.WriteLine($"{message} : {ex}");
-        }
-
         public void Log(string message)
         {
             Log(LogLevel.Information, message);
--- src/Pulse.Core/Log/ILog.cs
+++ src/Pulse.Core/Log/ILog.cs
@@ -8,12 +8,6 @@ namespace Pulse.Core.Log
 {
     public interface ILog
     {
-        void Debug(Exception ex, string message);
-        void Debug(string message);
-        void Error(Exception ex, string message);
-        void Warning(Exception ex, string message);
-        void Fatal(Exception ex, string message);
-        void Information(string message);
         void Log(LogLevel level, string message, Exception ex);
         void Log(LogLevel level, string message);
         void Log(string message);
